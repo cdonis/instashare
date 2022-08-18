a7f78ef79a1b7a12f238b2063c998976dfc89fc0
Carlos Donis <cdonisdiaz@gmail.com>
2022-08-18 15:03:29 -0400

    set hash navigation

--- src/.umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from 'D:/Work/Cuban Engineers/Recruitment process/Technical test/Application/instashare/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu','getInitialState','initialStateConfig','locale','layout','layoutActionRef','request',],
-});
-
-export { plugin };
--- src/.umi/plugin-helmet/exports.ts
+++
@@ -1,3 +0,0 @@
-// @ts-nocheck
-// @ts-ignore
-export { Helmet } from 'D:/Work/Cuban Engineers/Recruitment process/Technical test/Application/instashare/node_modules/react-helmet';
